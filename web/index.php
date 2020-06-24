9d122af7770adc66de4e31b8ac455b4d439c34d4
MartinH <smartin@bariloche.com.ar>
2020-06-24 19:17:35 -0300

    Merge origin/master
    Conflicts:
    	.gitignore

--- web/index.php
+++ web/index.php
@@ -4,8 +4,13 @@
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
 
+//preuba git
+
+
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
+
+
 
 $config = require __DIR__ . '/../config/web.php';
 
