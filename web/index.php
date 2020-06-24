1ec535a411a42da7f671b25fc7a28e17f92cd381
MartinH <smartin@bariloche.com.ar>
2020-06-24 19:44:39 -0300

    pc 1

--- web/index.php
+++ web/index.php
@@ -6,6 +6,8 @@ defined('YII_ENV') or define('YII_ENV', 'dev');
 
 //prueba git
 
+//dsdddddddd
+
 
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
